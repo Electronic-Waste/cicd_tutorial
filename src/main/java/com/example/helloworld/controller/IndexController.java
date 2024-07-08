00358c80298d04b5451d1aadf01c20885ad8f9eb
Electronic-Waste <2690692950@qq.com>
2024-07-08 15:55:53 +0800

    feat: initial commit.

---
+++ src/main/java/com/example/helloworld/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class IndexController {
+
+    @GetMapping("/say")
+    public String say(){
+        return "Hello World";
+    }
+}
